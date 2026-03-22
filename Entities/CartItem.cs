ae216e5ee873508e3743d5eab2917794805bddc6
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-22 19:12:49 -0300

    feat: implement cart entity

---
+++ Entities/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace E_commerce_API.Entities
+{
+    public class CartItem
+    {
+        public int CartItemId { get; set; }
+        public int CartId { get; set; }
+        public Cart Cart { get; set; } = null!;
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
