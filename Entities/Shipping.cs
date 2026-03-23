ec876c83ba02bc1ea39726dcefa5e7fc0722f108
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-23 17:47:01 -0300

    feat: implement shipping entity

---
+++ Entities/Shipping.cs
@@ -0,0 +1,21 @@
+﻿using E_commerce_API.Enums;
+
+namespace E_commerce_API.Entities
+{
+    public class Shipping
+    {
+        public int ShippingId { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+        public string RecipientName { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string State { get; set; } = "";
+        public string City { get; set; } = "";
+        public string ZipCode { get; set; } = "";
+        public decimal ShippingCost { get; set; }
+        public string? TrackingCode { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public DateTime? DeliveredDate { get; set; }
+        public ShippingStatus Status { get; set; } = null!;
+    }
+}
