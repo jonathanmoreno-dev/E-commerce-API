517d16190b408e213734a7d1b1da8e75a592aa43
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-25 17:52:38 -0300

    refactor: change category-product relationship to many-to-many

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -8,8 +8,7 @@
         public string LongDescription { get; set; } = "";
         public decimal Price { get; set; }
         public int Stock { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; } = null!;
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
         public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
         public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
