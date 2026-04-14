21479fc48092e471b144af8f4b6059664cf76be5
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-04-14 14:23:02 -0300

    feat: add refund entity configuration

---
+++ src/Infrastructure/Configuration/RefundConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using E_commerce_API.src.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace E_commerce_API.src.Infrastructure.Configuration
+{
+    public class RefundConfiguration : IEntityTypeConfiguration<Refund>
+    {
+        public void Configure(EntityTypeBuilder<Refund> builder)
+        {
+            builder.HasKey(x => x.RefundId);
+            builder.Property(x => x.Amount).HasPrecision(18, 2);
+            builder.HasOne(x => x.OrderItem).WithMany(y => y.Refunds).HasForeignKey(x => x.OrderItemId);
+            builder.HasIndex(x => x.OrderItemId).IsUnique();
+        }
+    }
+}
