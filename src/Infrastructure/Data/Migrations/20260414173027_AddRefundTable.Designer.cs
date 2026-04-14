4a90112b27f2f72eb47675dcd7b7892115f544d9
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-04-14 14:32:23 -0300

    feat: create Refund table

--- src/Infrastructure/Data/Migrations/20260414173027_AddRefundTable.Designer.cs
+++ src/Infrastructure/Data/Migrations/20260414173027_AddRefundTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using E_commerce_API.src.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace E_commerce_API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260414173027_AddRefundTable")]
+    partial class AddRefundTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Data/Migrations/20260414173027_AddRefundTable.cs
@@ -0,0 +1,479 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace E_commerce_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefundTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cart_User_UserId",
+                table: "Cart");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItem_Cart_CartId",
+                table: "CartItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItem_Products_ProductId",
+                table: "CartItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Orders_OrderId",
+                table: "OrderItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Products_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_User_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payment_Orders_OrderId",
+                table: "Payment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shipping_Orders_OrderId",
+                table: "Shipping");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Shipping",
+                table: "Shipping");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payment",
+                table: "Payment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItem",
+                table: "CartItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Shipping",
+                newName: "Shippings");
+
+            migrationBuilder.RenameTable(
+                name: "Payment",
+                newName: "Payments");
+
+            migrationBuilder.RenameTable(
+                name: "OrderItem",
+                newName: "OrderItems");
+
+            migrationBuilder.RenameTable(
+                name: "CartItem",
+                newName: "CartItems");
+
+            migrationBuilder.RenameTable(
+                name: "Cart",
+                newName: "Carts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_Email",
+                table: "Users",
+                newName: "IX_Users_Email");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Shipping_OrderId",
+                table: "Shippings",
+                newName: "IX_Shippings_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payment_OrderId",
+                table: "Payments",
+                newName: "IX_Payments_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItem_ProductId",
+                table: "OrderItems",
+                newName: "IX_OrderItems_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItem_OrderId",
+                table: "OrderItems",
+                newName: "IX_OrderItems_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItem_ProductId",
+                table: "CartItems",
+                newName: "IX_CartItems_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItem_CartId",
+                table: "CartItems",
+                newName: "IX_CartItems_CartId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cart_UserId",
+                table: "Carts",
+                newName: "IX_Carts_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Shippings",
+                table: "Shippings",
+                column: "ShippingId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments",
+                column: "PaymentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderItems",
+                table: "OrderItems",
+                column: "OrderItemId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                column: "CartItemId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts",
+                column: "CartId");
+
+            migrationBuilder.CreateTable(
+                name: "Refund",
+                columns: table => new
+                {
+                    RefundId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderItemId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    RefundDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Refund", x => x.RefundId);
+                    table.ForeignKey(
+                        name: "FK_Refund_OrderItems_OrderItemId",
+                        column: x => x.OrderItemId,
+                        principalTable: "OrderItems",
+                        principalColumn: "OrderItemId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Refund_OrderItemId",
+                table: "Refund",
+                column: "OrderItemId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "CartId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Orders_OrderId",
+                table: "Payments",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shippings_Orders_OrderId",
+                table: "Shippings",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Orders_OrderId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shippings_Orders_OrderId",
+                table: "Shippings");
+
+            migrationBuilder.DropTable(
+                name: "Refund");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Shippings",
+                table: "Shippings");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderItems",
+                table: "OrderItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameTable(
+                name: "Shippings",
+                newName: "Shipping");
+
+            migrationBuilder.RenameTable(
+                name: "Payments",
+                newName: "Payment");
+
+            migrationBuilder.RenameTable(
+                name: "OrderItems",
+                newName: "OrderItem");
+
+            migrationBuilder.RenameTable(
+                name: "Carts",
+                newName: "Cart");
+
+            migrationBuilder.RenameTable(
+                name: "CartItems",
+                newName: "CartItem");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Email",
+                table: "User",
+                newName: "IX_User_Email");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Shippings_OrderId",
+                table: "Shipping",
+                newName: "IX_Shipping_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Payments_OrderId",
+                table: "Payment",
+                newName: "IX_Payment_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItems_ProductId",
+                table: "OrderItem",
+                newName: "IX_OrderItem_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItem",
+                newName: "IX_OrderItem_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Carts_UserId",
+                table: "Cart",
+                newName: "IX_Cart_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItem",
+                newName: "IX_CartItem_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItems_CartId",
+                table: "CartItem",
+                newName: "IX_CartItem_CartId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Shipping",
+                table: "Shipping",
+                column: "ShippingId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payment",
+                table: "Payment",
+                column: "PaymentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem",
+                column: "OrderItemId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart",
+                column: "CartId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItem",
+                table: "CartItem",
+                column: "CartItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cart_User_UserId",
+                table: "Cart",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItem_Cart_CartId",
+                table: "CartItem",
+                column: "CartId",
+                principalTable: "Cart",
+                principalColumn: "CartId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItem_Products_ProductId",
+                table: "CartItem",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Orders_OrderId",
+                table: "OrderItem",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Products_ProductId",
+                table: "OrderItem",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_User_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payment_Orders_OrderId",
+                table: "Payment",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shipping_Orders_OrderId",
+                table: "Shipping",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
