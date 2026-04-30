3866d479eb80728a84c3ca9660018e6068c32602
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-04-30 13:42:25 -0300

    refactor: enrich Product and Category entities

--- src/Domain/Entities/Category.cs
+++ src/Domain/Entities/Category.cs
@@ -1,10 +1,45 @@
-﻿namespace E_commerce_API.src.Domain.Entities
+﻿using E_commerce_API.src.Domain.ValueObjects;
+
+namespace E_commerce_API.src.Domain.Entities
 {
     public class Category
     {
-        public int CategoryId { get; set; }
-        public string Name { get; set; } = "";
-        public string Description { get; set; } = "";
-        public ICollection<Product> Products { get; set; } = new List<Product>();
+        public int CategoryId { get; private set; }
+        public CategoryName Name { get; private set; } = null!;
+        public string Description { get; private set; } = "";
+
+        private readonly List<Product> _products = new();
+        public IReadOnlyCollection<Product> Products => _products;
+
+        private Category() { }
+        public Category(string name)
+        {
+            ChangeName(name);
+        }
+        public void ChangeName(string name)
+        {
+            Name = new CategoryName(name);
+        }
+        public void SetDescription(string description)
+        {
+            Description = description;
+        }
+        public void AddProduct(Product product)
+        {
+            if (product is null)
+                throw new ArgumentNullException("Product cannot be null");
+            if (_products.Any(x => x.ProductId == product.ProductId))
+                throw new ArgumentException($"Product with this Id: {product.ProductId} already in category");
+
+            _products.Add(product);
+        }
+        public void RemoveProduct(int productId)
+        {
+            var product = _products.FirstOrDefault(x => x.ProductId == productId);
+            if(product is null)
+                throw new ArgumentException($"Product not found with this Id: {productId}");
+
+            _products.Remove(product);
+        }
     }
 }
--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -1,15 +1,87 @@
-﻿namespace E_commerce_API.src.Domain.Entities
+﻿using System.Xml.Linq;
+using E_commerce_API.src.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace E_commerce_API.src.Domain.Entities
 {
     public class Product
     {
         public int ProductId { get; set; }
         public string Name { get; set; } = "";
         public string ShortDescription { get; set; } = "";
-        public string LongDescription { get; set; } = "";
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public ICollection<Category> Categories { get; set; } = new List<Category>();
-        public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
-        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public string LongDescription { get; private set; } = "";
+        public Money Price { get; private set; } = null!;
+        public Quantity Stock { get; private set; } = null!;
+
+        private List<Category> _categories = new();
+        public IReadOnlyCollection<Category> Categories => _categories;
+        
+        private List<CartItem> _cartItems = new();
+        public IReadOnlyCollection<CartItem> CartItems => _cartItems;
+
+        private List<OrderItem> _orderItems = new();
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        private Product() { }
+        public Product(string name, string shortDescription, string longDescription, decimal price, int stock)
+        {
+            SetName(name);
+            SetShortDescription(shortDescription);
+            SetLongDescription(longDescription);
+            Price = new Money(price);
+            Stock = new Quantity(stock);
+        }
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Product's name cannot be empty");
+            if (name.Length > 255)
+                throw new ArgumentException("Product's name cannot exceed 255 characters");
+
+            Name = name;
+        }
+        public void SetShortDescription(string shortDescription)
+        {
+            if (string.IsNullOrWhiteSpace(shortDescription))
+                throw new ArgumentException("Product's ShortDescription cannot be empty");
+            if (shortDescription.Length > 400)
+                throw new ArgumentException("Product's ShortDescription cannot exceed 400 characters");
+
+            ShortDescription = shortDescription;
+        }
+        public void SetLongDescription(string longDescription)
+        {
+            if (string.IsNullOrWhiteSpace(longDescription))
+                throw new ArgumentException("Product's LongDescription cannot be empty");
+
+            LongDescription = longDescription;
+        }
+        public void IncreaseStock(int stock)
+        {
+            Stock = Stock.Add(stock);
+        }
+        public void DecreaseStock(int stock)
+        {
+            Stock = Stock.Remove(stock);
+        }
+        public void AddCategory(Category category)
+        {
+            if (category is null)
+                throw new ArgumentNullException("Category cannot be null");
+            if (_categories.Any(x => x.CategoryId == category.CategoryId))
+                throw new ArgumentException($"Category with this Id: {category.CategoryId} already in product");
+
+            _categories.Add(category);
+            category.AddProduct(this);
+        }
+        public void RemoveCategory(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+            if (category is null)
+                throw new ArgumentNullException("Category cannot be null");
+
+            _categories.Remove(category);
+            category.RemoveProduct(ProductId);
+        }
     }
 }
