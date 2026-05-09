8f471674e975d44cbbf41989d6208987f4b7d157
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-09 12:14:56 -0300

    feat: create and implement Refund entity

--- src/Domain/Entities/Refund.cs
+++ src/Domain/Entities/Refund.cs
@@ -1,11 +1,20 @@
-﻿namespace E_commerce_API.src.Domain.Entities
+﻿using E_commerce_API.src.Domain.ValueObjects;
+
+namespace E_commerce_API.src.Domain.Entities
 {
     public class Refund
     {
-        public int RefundId { get; set; }
-        public int OrderItemId { get; set; }
-        public OrderItem OrderItem { get; set; } = null!;
-        public int Quantity { get; set; }
-        public DateTime RefundDate { get; set; }
+        public int RefundId { get; private set; }
+        public int OrderItemId { get; private set; }
+        public OrderItem OrderItem { get; private set; } = null!;
+        public Quantity Quantity { get; private set; } = null!;
+        public DateTime RefundDate { get; private set; }
+
+        private Refund() { }
+        public Refund(int quantity)
+        {
+            Quantity = new Quantity(quantity);
+            RefundDate = DateTime.UtcNow;
+        }
     }
 }
