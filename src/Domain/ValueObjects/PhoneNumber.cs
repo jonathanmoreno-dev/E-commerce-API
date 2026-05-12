88927cdeb7752d23a8689c0d293aafb2fc661f07
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-11 21:21:45 -0300

    feat: add PhoneNumber value object

--- src/Domain/ValueObjects/PhoneNumber.cs
+++ src/Domain/ValueObjects/PhoneNumber.cs
@@ -1,6 +1,17 @@
 ﻿namespace E_commerce_API.src.Domain.ValueObjects
 {
-    public class PhoneNumber
+    public record PhoneNumber
     {
+        public string Value { get; } = null!;
+        private PhoneNumber() { }
+        public PhoneNumber(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Phone number cannot be empty", nameof(value));
+            if (value.Length > 50)
+                throw new ArgumentException("Phone number exceed 50 characters", nameof(value));
+
+            Value = value;
+        }
     }
 }
