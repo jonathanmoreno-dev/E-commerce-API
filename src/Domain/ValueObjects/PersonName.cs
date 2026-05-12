c39cb5e211f736ec61a92934c691c965152ae0cb
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-11 21:18:45 -0300

    feat: add PersonName value object

--- src/Domain/ValueObjects/PersonName.cs
+++ src/Domain/ValueObjects/PersonName.cs
@@ -1,6 +1,17 @@
 ﻿namespace E_commerce_API.src.Domain.ValueObjects
 {
-    public class PersonName
+    public record PersonName
     {
+        public string Value { get; } = null!;
+        private PersonName() { }
+        public PersonName(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Person name cannot be empty", nameof(value));
+            if (value.Length > 150)
+                throw new ArgumentException("Person name exceed 150 characters", nameof(value));
+
+            Value = value;
+        }
     }
 }
