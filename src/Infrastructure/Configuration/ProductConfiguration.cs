741f7a1cae05295bf99faa42de34615dacd5d8c7
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-14 18:33:30 -0300

    refactor: configure Name, ShortDescription, LongDescription, Price and stock as complex properties

--- src/Infrastructure/Configuration/ProductConfiguration.cs
+++ src/Infrastructure/Configuration/ProductConfiguration.cs
@@ -9,9 +9,11 @@ namespace E_commerce_API.src.Infrastructure.Configuration
         public void Configure(EntityTypeBuilder<Product> builder)
         {
             builder.HasKey(x => x.ProductId);
-            builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
-            builder.Property(x => x.ShortDescription).IsRequired().HasMaxLength(400);
-            builder.Property(x => x.LongDescription).IsRequired();
+            builder.ComplexProperty(x => x.Name, p => p.Property(v => v.Value).HasColumnName("Name").IsRequired().HasMaxLength(255));
+            builder.ComplexProperty(x => x.ShortDescription, p => p.Property(v => v.Value).HasColumnName("ShortDescription").IsRequired().HasMaxLength(400));
+            builder.ComplexProperty(x => x.LongDescription, p => p.Property(v => v.Value).HasColumnName("LongDescription").IsRequired());
+            builder.ComplexProperty(x => x.Price, p => p.Property(v => v.Value).HasColumnName("Price").HasPrecision(18, 2));
+            builder.ComplexProperty(x => x.Stock, p => p.Property(v => v.Value).HasColumnName("Stock"));
             builder.Property(x => x.Price).HasPrecision(18, 2);
             builder.HasMany(x => x.Categories).WithMany(y => y.Products);
         }
