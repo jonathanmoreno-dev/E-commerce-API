a2fb6a52c6e8b11237c23a4b81299b65c915476f
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-14 18:36:05 -0300

    refactor: configure Quantity as complex property

--- src/Infrastructure/Configuration/RefundConfiguration.cs
+++ src/Infrastructure/Configuration/RefundConfiguration.cs
@@ -9,7 +9,7 @@ namespace E_commerce_API.src.Infrastructure.Configuration
         public void Configure(EntityTypeBuilder<Refund> builder)
         {
             builder.HasKey(x => x.RefundId);
-            builder.Property(x => x.Amount).HasPrecision(18, 2);
+            builder.ComplexProperty(x => x.Quantity, p => p.Property(v => v.Value).HasColumnName("Quantity"));
             builder.HasOne(x => x.OrderItem).WithMany(y => y.Refunds).HasForeignKey(x => x.OrderItemId);
             builder.HasIndex(x => x.OrderItemId).IsUnique();
         }
