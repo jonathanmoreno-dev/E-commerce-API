7ca8af9dd4482949995b3634c7d139a133996b30
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-14 18:52:33 -0300

    refactor: configure ShippingAddress and ShippingCost as complex properties

--- src/Infrastructure/Configuration/ShippingConfiguration.cs
+++ src/Infrastructure/Configuration/ShippingConfiguration.cs
@@ -12,13 +12,19 @@ namespace E_commerce_API.src.Infrastructure.Configuration
 
             builder.HasOne(x => x.Order).WithOne(y => y.Shipping).HasForeignKey<Shipping>(x => x.OrderId);
 
-            builder.Property(x => x.RecipientName).IsRequired().HasMaxLength(100);
-            builder.Property(x => x.Address).IsRequired().HasMaxLength(200);
-            builder.Property(x => x.State).IsRequired().HasMaxLength(50);
-            builder.Property(x => x.City).IsRequired().HasMaxLength(100);
-            builder.Property(x => x.ZipCode).IsRequired().HasMaxLength(20);
+            builder.ComplexProperty(x => x.ShippingCost, p => p.Property(v => v.Value).HasColumnName("ShippingCost").HasPrecision(18, 2));
 
-            builder.Property(x => x.ShippingCost).HasPrecision(10, 2);
+            builder.ComplexProperty(x => x.ShippingAddress, p =>
+            {
+                p.Property(r => r.RecipientName).HasColumnName("RecipientName").IsRequired().HasMaxLength(100);
+                p.Property(r => r.PhoneNumber).HasColumnName("PhoneNumber").IsRequired().HasMaxLength(20);
+                p.Property(r => r.Street).HasColumnName("Neighborhood").IsRequired().HasMaxLength(100);
+                p.Property(r => r.Number).HasColumnName("Number").IsRequired().HasMaxLength(50);
+                p.Property(r => r.State).HasColumnName("State").IsRequired().HasMaxLength(20);
+                p.Property(r => r.City).HasColumnName("City").IsRequired().HasMaxLength(50);
+                p.Property(r => r.ZipCode).HasColumnName("ZipCode").IsRequired().HasMaxLength(20);
+            });
+
             builder.Property(x => x.TrackingCode).HasMaxLength(100);
             builder.Property(x => x.Status).HasConversion<string>();
             builder.HasIndex(x => x.OrderId).IsUnique();
