0ddc48d509ff916912a48c8be8732fd8a2754975
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-14 17:02:36 -0300

    feat: add Email value object

---
+++ src/Domain/ValueObjects/Email.cs
@@ -0,0 +1,17 @@
+﻿namespace E_commerce_API.src.Domain.ValueObjects
+{
+    public record Email
+    {
+        public string Value { get; } = null!;
+        private Email() { }
+        public Email(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Email cannot be empty", nameof(value));
+            if (value.Length > 255)
+                throw new ArgumentException("Email cannot exceed 255 characters", nameof(value));
+
+            Value = value;
+        }
+    }
+}
