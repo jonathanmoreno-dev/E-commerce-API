cf1900f76e0c1a382c1b24598560b79345aa0769
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-14 16:55:36 -0300

    feat: add CartId foreign key property

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -7,6 +7,7 @@
         public string Email { get; set; } = "";
         public string Phone { get; set; } = "";
         public string PasswordHash { get; set; } = "";
+        public int CartId { get; set; }
         public Cart Cart { get; set; } = null!;
         public ICollection<Order> Orders { get; set; } = new List<Order>();
         public bool IsAdmin { get; set; }
