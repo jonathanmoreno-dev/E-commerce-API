261559594773fe2d29aff112151db62006e63f6f
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-14 19:00:59 -0300

    feat: configure default timestamp for RefundDate

--- src/Infrastructure/Configuration/RefundConfiguration.cs
+++ src/Infrastructure/Configuration/RefundConfiguration.cs
@@ -10,6 +10,7 @@ namespace E_commerce_API.src.Infrastructure.Configuration
         {
             builder.HasKey(x => x.RefundId);
             builder.ComplexProperty(x => x.Quantity, p => p.Property(v => v.Value).HasColumnName("Quantity"));
+            builder.Property(x => x.RefundDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
             builder.HasOne(x => x.OrderItem).WithMany(y => y.Refunds).HasForeignKey(x => x.OrderItemId);
             builder.HasIndex(x => x.OrderItemId).IsUnique();
         }
