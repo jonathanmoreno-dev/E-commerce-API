d3ee2b5426b3d5fd675de9b80a61ccca8f81cb14
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-18 15:27:21 -0300

    feat: add Order navigation property

--- src/Domain/Entities/Shipping.cs
+++ src/Domain/Entities/Shipping.cs
@@ -7,6 +7,7 @@ namespace E_commerce_API.src.Domain.Entities
     {
         public int ShippingId { get; private set; }
         public int OrderId { get; private set; }
+        public Order Order { get; private set; } = null!;
         public ShippingAddress ShippingAddress { get; private set; } = null!;
         public Money ShippingCost { get; private set; } = null!;
         public string? TrackingCode { get; private set; }
