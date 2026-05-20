7ff282a53259faa22ba27183236f452cd5f73f64
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-20 14:31:59 -0300

    feat: add CartDetailsDTO

--- src/Application/DTOs/CartDTOs/CartDetailsDTO.cs
+++ src/Application/DTOs/CartDTOs/CartDetailsDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace E_commerce_API.src.Application.DTOs.CartDTOs
+﻿using E_commerce_API.src.Application.DTOs.CartItemDTOs;
+using E_commerce_API.src.Application.DTOs.UserDTOs;
+
+namespace E_commerce_API.src.Application.DTOs.CartDTOs
 {
     public class CartDetailsDTO
     {
+        public int CartId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public UserListDTO UserListDTO { get; set; } = null!;
+        public List<CartItemListDTO> CartItemListDTOs { get; set; } = new();
     }
 }
