8918bf519d9de5a6cf08689ab8f3af0bff51cf5e
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-20 18:40:39 -0300

    feat: implement OrderDTOs

--- src/Application/DTOs/OrderDTOs/OrderDetailsDTO.cs
+++ src/Application/DTOs/OrderDTOs/OrderDetailsDTO.cs
@@ -1,6 +1,19 @@
-﻿namespace E_commerce_API.src.Application.DTOs.OrderDTOs
+﻿using E_commerce_API.src.Application.DTOs.OrderItemDTOs;
+using E_commerce_API.src.Application.DTOs.PaymentDTOs;
+using E_commerce_API.src.Application.DTOs.ShippingDTOs;
+using E_commerce_API.src.Application.DTOs.UserDTOs;
+
+namespace E_commerce_API.src.Application.DTOs.OrderDTOs
 {
     public class OrderDetailsDTO
     {
+        public int OrderId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = "";
+        public DateTime CreateAt { get; set; }
+        public ShippingListDTO ShippingListDTO { get; set; } = null!;
+        public List<PaymentListDTO> PaymentListDTOs { get; set; } = new();
+        public List<OrderItemListDTO> OrderItemListDTOs { get; set; } = new();
+        public UserListDTO UserListDTO { get; set; } = null!;
     }
 }
--- src/Application/DTOs/OrderDTOs/OrderListDTO.cs
+++ src/Application/DTOs/OrderDTOs/OrderListDTO.cs
@@ -1,6 +1,10 @@
-﻿namespace E_commerce_API.src.Application.DTOs.OrderDTOs
+﻿using E_commerce_API.src.Application.DTOs.UserDTOs;
+
+namespace E_commerce_API.src.Application.DTOs.OrderDTOs
 {
     public class OrderListDTO
     {
+        public int OrderId { get; set; }
+        public UserListDTO UserListDTO { get; set; } = null!;
     }
 }
