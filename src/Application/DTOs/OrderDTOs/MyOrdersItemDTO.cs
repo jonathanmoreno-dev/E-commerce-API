c9b3e0a819bca761f117a22dc747adb57d5f83f4
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-25 15:26:52 -0300

    feat: improve MyOrdersItemDTO

--- src/Application/DTOs/OrderDTOs/MyOrdersItemDTO.cs
+++ src/Application/DTOs/OrderDTOs/MyOrdersItemDTO.cs
@@ -1,16 +1,14 @@
-﻿using E_commerce_API.src.Application.DTOs.ProductDTOs;
+﻿using E_commerce_API.src.Application.DTOs.OrderItemDTOs;
+using E_commerce_API.src.Application.DTOs.ProductDTOs;
+using E_commerce_API.src.Domain.Enums;
 
 namespace E_commerce_API.src.Application.DTOs.OrderDTOs
 {
     public class MyOrdersItemDTO
     {
         public int OrderId { get; set; }
-        public int OrderItemId { get; set; }
         public DateTime CreatedAt { get; set; }
-        public string Status { get; set; } = "";
-        public ProductSummaryDTO ProductSummaryDTO { get; set; } = null!;
-        public decimal UnitPrice { get; set; }
-        public int Quantity { get; set; }
-        public decimal Total { get; set; }
+        public OrderStatus Status { get; set; }
+        public OrderItemSummaryDTO OrderItemListDTO { get; set; } = null!;
     }
 }
