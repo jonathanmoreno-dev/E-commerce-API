25f770dfc61b00d40ade3951a36ab42960076422
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-25 14:45:04 -0300

    feat: improve OrderDetailsDTO

--- src/Application/DTOs/OrderDTOs/OrderDetailsDTO.cs
+++ src/Application/DTOs/OrderDTOs/OrderDetailsDTO.cs
@@ -1,19 +1,18 @@
 ﻿using E_commerce_API.src.Application.DTOs.OrderItemDTOs;
 using E_commerce_API.src.Application.DTOs.PaymentDTOs;
 using E_commerce_API.src.Application.DTOs.ShippingDTOs;
-using E_commerce_API.src.Application.DTOs.UserDTOs;
 
 namespace E_commerce_API.src.Application.DTOs.OrderDTOs
 {
     public class OrderDetailsDTO
     {
         public int Id { get; set; }
-        public decimal TotalAmount { get; set; }
+        public decimal Total { get; set; } 
         public string Status { get; set; } = "";
-        public DateTime CreateAt { get; set; }
-        public ShippingListDTO ShippingListDTO { get; set; } = null!;
-        public List<PaymentListDTO> PaymentListDTOs { get; set; } = new();
-        public List<OrderItemListDTO> OrderItemListDTOs { get; set; } = new();
-        public UserListDTO UserListDTO { get; set; } = null!;
+        public decimal ShippingCost { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ShippingSummaryDTO? ShippingSummaryDTO { get; set; } = null!;
+        public PaymentSummaryDTO? PaymentSummaryDTO { get; set; } = null!;
+        public List<OrderItemDetailsDTO> OrderItemListDTOs { get; set; } = new();
     }
 }
