3e31c544031d137fcd12fe8d50f5d22f842bc5e3
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-25 14:38:52 -0300

    feat: add and implement MyOrdersItemDTO

---
+++ src/Application/DTOs/OrderDTOs/MyOrdersItemDTO.cs
@@ -0,0 +1,16 @@
+﻿using E_commerce_API.src.Application.DTOs.ProductDTOs;
+
+namespace E_commerce_API.src.Application.DTOs.OrderDTOs
+{
+    public class MyOrdersItemDTO
+    {
+        public int OrderId { get; set; }
+        public int OrderItemId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Status { get; set; } = "";
+        public ProductSummaryDTO ProductSummaryDTO { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+    }
+}
