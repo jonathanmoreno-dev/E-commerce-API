9ff054875fd1b5c2ccfcb503aa69eeb012b09088
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-25 14:39:26 -0300

    feat: add and implement ShippingDTOs

--- src/Application/DTOs/ShippingDTOs/ShippingListDTO.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace E_commerce_API.src.Application.DTOs.ShippingDTOs
-{
-    public class ShippingListDTO
-    {
-    }
-}
--- src/Application/DTOs/ShippingDTOs/ShippingSummaryDTO.cs
+++ src/Application/DTOs/ShippingDTOs/ShippingSummaryDTO.cs
@@ -2,15 +2,12 @@
 
 namespace E_commerce_API.src.Application.DTOs.ShippingDTOs
 {
-    public class ShippingDetailsDTO
+    public class ShippingSummaryDTO
     {
         public int Id { get; set; }
         public string RecipientName { get; set; } = "";
         public string PhoneNumber { get; set; } = "";
         public string FullAddress { get; set; } = "";
-        public string? TrackingCode { get; set; } 
-        public DateTime? ShippedAt { get; set; }
-        public DateTime? DeliveredAt { get; set; }
         public ShippingStatus Status { get; set; }
     }
 }
