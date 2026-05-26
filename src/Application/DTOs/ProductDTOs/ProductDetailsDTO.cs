c61c00e8b7cd46aba85bee383dc59e58822e03cd
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-26 15:36:04 -0300

    feat: implement ProductDetailsDTO

--- src/Application/DTOs/ProductDTOs/ProductDetailsDTO.cs
+++ src/Application/DTOs/ProductDTOs/ProductDetailsDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace E_commerce_API.src.Application.DTOs.ProductDTOs
+﻿using E_commerce_API.src.Application.DTOs.CategoryDTOs;
+
+namespace E_commerce_API.src.Application.DTOs.ProductDTOs
 {
     public class ProductDetailsDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string ShortDescription { get; set; } = "";
+        public string LongDescription { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public List<CategoryListDTO> CategoryListDTOs { get; set; } = new();
     }
 }
