36506f59824a8906ec26030ba0230df2b31a18d0
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-27 18:03:00 -0300

    feat: add CategoryImage and ProductImage value objects

---
+++ src/Domain/ValueObjects/ProductImage.cs
@@ -0,0 +1,19 @@
+﻿namespace E_commerce_API.src.Domain.ValueObjects
+{
+    public record ProductImage
+    {
+        public string Url { get; } = null!;
+        public int Order { get; }
+        private ProductImage() { }
+        public ProductImage(string url, int order)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                throw new ArgumentException("ProductImageUrl cannot be empty", nameof(url));
+            if (order <= 0)
+                throw new ArgumentOutOfRangeException(nameof(order), "Order must be greater than 0");
+
+            Url = url;
+            Order = order;
+        }
+    }
+}
