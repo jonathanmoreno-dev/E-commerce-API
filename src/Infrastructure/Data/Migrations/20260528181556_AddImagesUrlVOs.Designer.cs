d0b468644a5359c0f364011664d5de9cd7c3ea81
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-28 15:19:11 -0300

    refactor: make migrations to database

--- src/Infrastructure/Data/Migrations/20260528181556_AddImagesUrlVOs.Designer.cs
+++ src/Infrastructure/Data/Migrations/20260528181556_AddImagesUrlVOs.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using E_commerce_API.src.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace E_commerce_API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260528181556_AddImagesUrlVOs")]
+    partial class AddImagesUrlVOs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Data/Migrations/20260528181556_AddImagesUrlVOs.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace E_commerce_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagesUrlVOs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "avatar_image_url",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "category_image_url",
+                table: "categories",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ProductImage",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    product_image_url = table.Column<string>(type: "text", nullable: false),
+                    product_image_order = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImage", x => new { x.ProductId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_ProductImage_products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductImage");
+
+            migrationBuilder.DropColumn(
+                name: "avatar_image_url",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "category_image_url",
+                table: "categories");
+        }
+    }
+}
