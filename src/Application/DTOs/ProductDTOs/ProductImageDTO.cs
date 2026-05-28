d6bc51bb78040d23d21b8e527d1692699513731a
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-05-28 17:18:40 -0300

    feat: add ProductImageDTO

---
+++ src/Application/DTOs/ProductDTOs/ProductImageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace E_commerce_API.src.Application.DTOs.ProductDTOs
+{
+    public class ProductImageDTO
+    {
+        public string Url { get; set; } = "";
+        public int Order { get; set; }
+    }
+}
