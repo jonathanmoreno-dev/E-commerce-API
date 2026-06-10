70d1a2c7fa15ba4ad233d334d81982bab8f95dcc
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-10 17:34:55 -0300

    feat(order): map shipping and payment properties

--- src/Infrastructure/Configuration/OrderConfiguration.cs
+++ src/Infrastructure/Configuration/OrderConfiguration.cs
@@ -11,6 +11,21 @@ namespace E_commerce_API.src.Infrastructure.Configuration
             builder.ToTable("orders");
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).HasColumnName("id");
+            builder.ComplexProperty(x => x.ShippingAddress, p =>
+            {
+                p.ComplexProperty(r => r.RecipientName, pn => pn.Property(v => v.Value).HasColumnName("recipient_name").IsRequired().HasMaxLength(100));
+                p.ComplexProperty(r => r.PhoneNumber, pn => pn.Property(v => v.Value).HasColumnName("phone_number").IsRequired().HasMaxLength(20));
+                p.Property(r => r.Neighborhood).HasColumnName("neighborhood").IsRequired().HasMaxLength(100);
+                p.Property(r => r.Street).HasColumnName("street").IsRequired().HasMaxLength(50);
+                p.Property(r => r.Number).HasColumnName("number").IsRequired().HasMaxLength(50);
+                p.Property(r => r.State).HasColumnName("state").IsRequired().HasMaxLength(20);
+                p.Property(r => r.City).HasColumnName("city").IsRequired().HasMaxLength(50);
+                p.Property(r => r.ZipCode).HasColumnName("zip_code").IsRequired().HasMaxLength(20);
+            });
+            builder.ComplexProperty(x => x.ShippingCost, p => p.Property(v => v.Value).HasColumnName("shipping_cost").HasPrecision(18, 2));
+            builder.Property(x => x.PaymentMethod).HasConversion<string>().HasColumnName("payment_method");
+            builder.Ignore(x => x.SubTotal);
+            builder.ComplexProperty(x => x.TotalPaid, p => p.Property(v => v.Value).HasColumnName("total_paid").HasPrecision(18,2));
             builder.Property(x => x.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP").HasColumnName("created_at");
             builder.Property(x => x.Status).HasConversion<string>().HasColumnName("status");
             builder.Property(x => x.UserId).HasColumnName("user_id");
