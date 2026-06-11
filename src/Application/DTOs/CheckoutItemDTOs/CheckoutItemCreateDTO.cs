3d574ff73ec2b8c822f4a5f1021e8d5ad3f8725d
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-11 19:25:06 -0300

    feat(checkout-item) add CheckoutItemCreateDTO

---
+++ src/Application/DTOs/CheckoutItemDTOs/CheckoutItemCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace E_commerce_API.src.Application.DTOs.CheckoutItemDTOs
+{
+    public class CheckoutItemCreateDTO
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
