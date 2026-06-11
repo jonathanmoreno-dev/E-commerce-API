d23e93691fcf60748f32adc1a9ed47d163129759
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-11 18:10:27 -0300

    feat(shipping-address) add ShippingAddress DTO

---
+++ src/Application/DTOs/ShippingDTOs/ShippingAddressDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace E_commerce_API.src.Application.DTOs.ShippingDTOs
+{
+    public class ShippingAddressDTO
+    {
+        public string RecipientName { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string Neighborhood { get; } = "";
+        public string Street { get; } = "";
+        public string Number { get; } = "";
+        public string State { get; } = "";
+        public string City { get; } = "";
+        public string ZipCode { get; } = "";
+    }
+}
