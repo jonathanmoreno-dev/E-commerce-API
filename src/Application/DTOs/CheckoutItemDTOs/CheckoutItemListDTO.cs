1dee67a59c1b4e03a0f349f88c5adcb7fa510fd9
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-11 19:38:17 -0300

    refactor(checkout): allow ProductSummaryDTO property to be nullable

--- src/Application/DTOs/CheckoutItemDTOs/CheckoutItemListDTO.cs
+++ src/Application/DTOs/CheckoutItemDTOs/CheckoutItemListDTO.cs
@@ -5,7 +5,7 @@ namespace E_commerce_API.src.Application.DTOs.CheckoutItemDTOs
     public class CheckoutItemListDTO
     {
         public int Id { get; set; }
-        public ProductSummaryDTO ProductSummaryDTO { get; set; }
+        public ProductSummaryDTO ProductSummaryDTO { get; set; } = null!;
         public decimal UnitPrice { get; set; }
         public int Quantity { get; set; }
     }
