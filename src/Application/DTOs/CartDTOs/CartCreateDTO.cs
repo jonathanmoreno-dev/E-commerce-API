d77158a32fa6216f036f55b73e9100c196bb474f
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-11 16:38:46 -0300

    feat(cart) add CartCreateDTO

---
+++ src/Application/DTOs/CartDTOs/CartCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce_API.src.Application.DTOs.CartDTOs
+{
+    public class CartCreateDTO
+    {
+        [Required]
+        public int UserId { get; set; }
+    }
+}
