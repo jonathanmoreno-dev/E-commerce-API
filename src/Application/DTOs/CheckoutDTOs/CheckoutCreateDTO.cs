62719ce237722155696e795e0a3e698bf7d59c3b
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-11 18:11:13 -0300

    feat(checkout) add CheckoutCreateDTO

---
+++ src/Application/DTOs/CheckoutDTOs/CheckoutCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using E_commerce_API.src.Application.DTOs.CheckoutItemDTOs;
+using E_commerce_API.src.Application.DTOs.ShippingDTOs;
+using E_commerce_API.src.Domain.Enums;
+
+namespace E_commerce_API.src.Application.DTOs.CheckoutDTOs
+{
+    public class CheckoutCreateDTO
+    {
+        public int UserId { get; set; }
+        public ShippingAddressDTO ShippingAddressDTO { get; set; } = null!;
+        public decimal ShippingCost { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public List<CheckoutItemCreateDTO> CheckoutItemCreateDTOs { get; set; } = new();
+    }
+}
