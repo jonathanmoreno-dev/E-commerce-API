a7886c1495800c1b72ebb32d5481ee6e0b07d6f2
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-16 19:42:41 -0300

    feat(order): add OrderCreateDTO

---
+++ src/Application/DTOs/OrderDTOs/OrderCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using E_commerce_API.src.Application.DTOs.OrderItemDTOs;
+using E_commerce_API.src.Application.DTOs.ShippingDTOs;
+using E_commerce_API.src.Domain.Enums;
+
+namespace E_commerce_API.src.Application.DTOs.OrderDTOs
+{
+    public class OrderCreateDTO
+    {
+        public ShippingAddressDTO ShippingAddressDTO { get; set; } = null!;
+        public decimal ShippingCost { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public List<OrderItemCreateDTO> OrderItemCreateDTOs { get; set; } = new();
+        public decimal TotalPaid { get; set; }
+    }
+}
