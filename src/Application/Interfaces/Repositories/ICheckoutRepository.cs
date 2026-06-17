b06aef2cc763b0f47eb5061589c1e2bbcb47d98a
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-17 14:54:03 -0300

    feat(checkout): add ICheckoutRepository interface

---
+++ src/Application/Interfaces/Repositories/ICheckoutRepository.cs
@@ -0,0 +1,13 @@
+﻿using E_commerce_API.src.Domain.Entities;
+
+namespace E_commerce_API.src.Application.Interfaces.Repositories
+{
+    public interface ICheckoutRepository
+    {
+        public Task<IEnumerable<Checkout>> GetAllAsync();
+        public Task<Checkout> GetByIdAsync(int id);
+        public void Add(Checkout checkout);
+        public void Update(Checkout checkout);
+        public void Remove(Checkout checkout);
+    }
+}
