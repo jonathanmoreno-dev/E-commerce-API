38366b6658cf1346bafb82af011dfc3093aafb2b
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-17 14:32:45 -0300

    feat(refund): add RefundCreateDTO

---
+++ src/Application/DTOs/RefundDTOs/RefundCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce_API.src.Application.DTOs.RefundDTOs
+{
+    public class RefundCreateDTO
+    {
+        public int Quantity { get; set; }
+    }
+}
