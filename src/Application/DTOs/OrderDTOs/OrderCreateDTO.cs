392c893ed2778aa85315c458890296854e02c249
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-17 14:34:34 -0300

    feat(order): add Required attribute in OrderCreateDTO

--- src/Application/DTOs/OrderDTOs/OrderCreateDTO.cs
+++ src/Application/DTOs/OrderDTOs/OrderCreateDTO.cs
@@ -1,4 +1,5 @@
-﻿using E_commerce_API.src.Application.DTOs.OrderItemDTOs;
+﻿using System.ComponentModel.DataAnnotations;
+using E_commerce_API.src.Application.DTOs.OrderItemDTOs;
 using E_commerce_API.src.Application.DTOs.ShippingDTOs;
 using E_commerce_API.src.Domain.Enums;
 
@@ -6,9 +7,11 @@ namespace E_commerce_API.src.Application.DTOs.OrderDTOs
 {
     public class OrderCreateDTO
     {
+        [Required]
         public ShippingAddressDTO ShippingAddressDTO { get; set; } = null!;
         public decimal ShippingCost { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
+        [Required]
         public List<OrderItemCreateDTO> OrderItemCreateDTOs { get; set; } = new();
         public decimal TotalPaid { get; set; }
     }
