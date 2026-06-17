028caadb50b997384642f34a5f7cf709d5c525af
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-17 15:00:15 -0300

    refactor(checkout): enable GetByIdAsync nullable return in repository

--- src/Application/Interfaces/Repositories/ICheckoutRepository.cs
+++ src/Application/Interfaces/Repositories/ICheckoutRepository.cs
@@ -5,7 +5,7 @@ namespace E_commerce_API.src.Application.Interfaces.Repositories
     public interface ICheckoutRepository
     {
         public Task<IEnumerable<Checkout>> GetAllAsync();
-        public Task<Checkout> GetByIdAsync(int id);
+        public Task<Checkout?> GetByIdAsync(int id);
         public void Add(Checkout checkout);
         public void Update(Checkout checkout);
         public void Remove(Checkout checkout);
