1496f5ba8bf9ca98e41b1e6ecb5d6c1139e32322
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-18 16:49:18 -0300

    feat(cart): add CartItemUpdateDTO

---
+++ src/Application/DTOs/CartItemDTOs/CartItemUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace E_commerce_API.src.Application.DTOs.CartItemDTOs
+{
+    public class CartItemUpdateDTO
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
