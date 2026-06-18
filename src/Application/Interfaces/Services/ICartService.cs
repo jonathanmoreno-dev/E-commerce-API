aa291a7254d5ddad7798b09d32a1b8e4acaea7a3
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-18 16:40:51 -0300

    feat(cart): add ICartService interface

---
+++ src/Application/Interfaces/Services/ICartService.cs
@@ -0,0 +1,17 @@
+﻿using E_commerce_API.src.Application.DTOs.CartDTOs;
+using E_commerce_API.src.Application.DTOs.CartItemDTOs;
+
+namespace E_commerce_API.src.Application.Interfaces.Services
+{
+    public interface ICartService
+    {
+        public Task<IEnumerable<CartListDTO>> GetAllCartsAsync();
+        public Task<CartDetailsDTO> GetCartByIdAsync(int id);
+        public Task<CartDetailsDTO> GetCurrentCartAsync();
+        public Task<CartDetailsDTO> CreateCartAsync();
+        public Task<CartDetailsDTO> AddItemAsync(CartItemCreateDTO item);
+        public Task<CartDetailsDTO> RemoveItemAsync(int productId);
+        public Task<CartDetailsDTO> UpdateItemQuantityAsync(int productId, int quantity);
+        public Task<CartDetailsDTO> ClearAsync(int id);
+    }
+}
