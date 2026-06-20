be26a939630fabab1097ea4740f8654c9a948489
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-20 15:33:27 -0300

    feat(checkout): add CheckoutSummaryDTO

---
+++ Ecommerce.Application/DTOs/CheckoutDTOs/CheckoutSummaryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.Application.DTOs.CheckoutItemDTOs;
+using Ecommerce.Application.DTOs.ProductDTOs;
+using Ecommerce.Domain.Enums;
+
+namespace Ecommerce.Application.DTOs.CheckoutDTOs
+{
+    public class CheckoutSummaryDTO
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public decimal Total { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public List<CheckoutItemListDTO> CheckoutItemListDTOs { get; set; } = new();
+    }
+}
