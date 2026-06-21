4e590b8de7a0f1d33230c7df0dbbff6356cd035c
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-21 12:16:35 -0300

    feat(checkout): add CheckoutUpdateDTO

---
+++ Ecommerce.Application/DTOs/CheckoutDTOs/CheckoutUpdateDTO.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce.Application.DTOs.ShippingDTOs;
+using Ecommerce.Domain.Enums;
+
+namespace Ecommerce.Application.DTOs.CheckoutDTOs
+{
+    public class CheckoutUpdateDTO
+    {
+        public ShippingAddressDTO? ShippingAddressDTO { get; set; } 
+        public PaymentMethod? PaymentMethod { get; set; }
+    }
+}
