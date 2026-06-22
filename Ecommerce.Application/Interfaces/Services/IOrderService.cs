1c894853d51a747cee5e73d8fdff040530fdf7fb
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-22 15:38:32 -0300

    feat(order): add IOrderService interface

--- Ecommerce.Application/Interfaces/Services/IOrderService.cs
+++ Ecommerce.Application/Interfaces/Services/IOrderService.cs
@@ -1,6 +1,23 @@
-﻿namespace Ecommerce.Application.Interfaces.Services
+﻿using Ecommerce.Application.DTOs.OrderDTOs;
+using Ecommerce.Application.DTOs.RefundDTOs;
+using Ecommerce.Domain.Enums;
+
+namespace Ecommerce.Application.Interfaces.Services
 {
     public interface IOrderService
     {
+        public Task<IEnumerable<OrderListItemDTO>> GetAllCurrentAsync();
+        public Task<IEnumerable<OrderListItemDTO>> GetAllByUserIdAsync(int userId);
+        public Task<IEnumerable<OrderListItemDTO>> GetAllByStatusAsync(OrderStatus status);
+        public Task<OrderDetailsDTO> GetByIdAsync(int id);
+        public Task<OrderDetailsDTO> CreateFromCheckoutAsync(int checkoutId);
+        public Task<OrderDetailsDTO> RefundItemAsync(RefundCreateDTO refundCreate);
+        public Task CancelOrderAsync(int orderId);
+        public Task CreateShippingAsync(int orderId);
+        public Task ProcessShippingAsync(int orderId);
+        public Task ShipShippingAsync(int orderId);
+        public Task ShippingInTransitAsync(int orderId);
+        public Task DeliverShippingAsync(int orderId);
+        public Task ReturnShippingAsync(int orderId);
     }
 }
