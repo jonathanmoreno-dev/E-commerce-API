c6435aaec59cd998a17a422eb0cf50a756d6e0c9
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-22 14:10:34 -0300

    feat(refund): add OrderId and OrderItemId properties to RefundCreateDTO

--- Ecommerce.Application/DTOs/RefundDTOs/RefundCreateDTO.cs
+++ Ecommerce.Application/DTOs/RefundDTOs/RefundCreateDTO.cs
@@ -4,6 +4,8 @@ namespace Ecommerce.Application.DTOs.RefundDTOs
 {
     public class RefundCreateDTO
     {
+        public int OrderId { get; set; }
+        public int OrderItemId { get; set; }
         public int Quantity { get; set; }
     }
 }
