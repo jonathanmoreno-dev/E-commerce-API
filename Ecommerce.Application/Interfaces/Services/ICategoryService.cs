dae3b392aa270fc16ff5e53357541f072d7f37e5
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-22 15:36:31 -0300

    refactor(category): remove entity name from ICategoryService methods

--- Ecommerce.Application/Interfaces/Services/ICategoryService.cs
+++ Ecommerce.Application/Interfaces/Services/ICategoryService.cs
@@ -4,10 +4,10 @@ namespace Ecommerce.Application.Interfaces.Services
 {
     public interface ICategoryService
     {
-        public Task<IEnumerable<CategoryListDTO>> GetAllCategoriesAsync();
-        public Task<CategoryDetailsDTO> GetCategoryByIdAsync(int id);
-        public Task<CategoryDetailsDTO> CreateCategoryAsync(CategoryCreateDTO categoryCreate);
-        public Task<CategoryDetailsDTO> UpdateCategoryAsync(int categoryId, CategoryUpdateDTO categoryUpdate);
-        public Task DeleteCategoryAsync(int id);
+        public Task<IEnumerable<CategoryListDTO>> GetAllAsync();
+        public Task<CategoryDetailsDTO> GetByIdAsync(int id);
+        public Task<CategoryDetailsDTO> CreateAsync(CategoryCreateDTO categoryCreate);
+        public Task<CategoryDetailsDTO> UpdateAsync(int categoryId, CategoryUpdateDTO categoryUpdate);
+        public Task DeleteAsync(int id);
     }
 }
