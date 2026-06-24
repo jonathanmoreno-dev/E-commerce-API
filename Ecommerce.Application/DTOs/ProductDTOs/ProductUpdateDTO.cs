1190f042813a83305bab92214ac794f2d4040abe
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-24 17:27:40 -0300

    feat(product): add ProductUpdateDTO

---
+++ Ecommerce.Application/DTOs/ProductDTOs/ProductUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.DTOs.ProductDTOs
+{
+    public class ProductUpdateDTO
+    {
+        public string? Name { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public decimal? Price { get; set; }
+        public int? Quantity { get; set; }
+    }
+}
