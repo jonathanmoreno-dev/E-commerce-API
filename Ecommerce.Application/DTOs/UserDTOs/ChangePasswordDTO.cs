8835432eb7c8468235ef3e61840d12a076d62e6f
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-25 21:14:38 -0300

    feat(user): add ChangePasswordDTO

---
+++ Ecommerce.Application/DTOs/UserDTOs/ChangePasswordDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Application.DTOs.UserDTOs
+{
+    public class ChangePasswordDTO
+    {
+        [Required]
+        public string CurrentPassword { get; set; } = "";
+        [Required]
+        public string NewPassword { get; set; } = "";
+    }
+}
