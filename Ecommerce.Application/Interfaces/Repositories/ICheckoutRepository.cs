61da285949c7ffc403d0aefe73d4aa836b2d9c2c
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-26 17:11:16 -0300

    feat(checkout): add GetAllByUserIdAsync method in ICheckoutRepository interface

--- Ecommerce.Application/Interfaces/Repositories/ICheckoutRepository.cs
+++ Ecommerce.Application/Interfaces/Repositories/ICheckoutRepository.cs
@@ -5,6 +5,7 @@ namespace Ecommerce.Application.Interfaces.Repositories
     public interface ICheckoutRepository
     {
         public Task<IEnumerable<Checkout>> GetAllAsync();
+        public Task<IEnumerable<Checkout>> GetAllByUserIdAsync(int userId);
         public Task<Checkout?> GetByIdAsync(int id);
         public void Add(Checkout checkout);
         public void Update(Checkout checkout);
