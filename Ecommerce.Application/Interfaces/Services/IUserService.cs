775e3b43b0800db36b6017be2b2aebd6a5a87f09
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-25 21:14:55 -0300

    feat(user): add IUserService interface methods

--- Ecommerce.Application/Interfaces/Services/IUserService.cs
+++ Ecommerce.Application/Interfaces/Services/IUserService.cs
@@ -1,6 +1,20 @@
-﻿namespace Ecommerce.Application.Interfaces.Services
+﻿using Ecommerce.Application.DTOs.ShippingDTOs;
+using Ecommerce.Application.DTOs.UserDTOs;
+
+namespace Ecommerce.Application.Interfaces.Services
 {
     public interface IUserService
     {
+        public Task<IEnumerable<UserListDTO>> GetAllAsync();
+        public Task<IEnumerable<UserSummaryDTO>> GetAllAdminsAsync();
+        public Task<IEnumerable<UserSummaryDTO>> GetAllStandardUsersAsync();
+        public Task<UserDetailsDTO> GetByIdAsync(int id);
+        public Task<UserDetailsDTO> GetCurrentAsync();
+        public Task<UserDetailsDTO> CreateAsync(UserCreateDTO userCreate);
+        public Task<UserDetailsDTO> UpdateAsync(int userId, UserUpdateDTO userUpdate);
+        public Task<UserDetailsDTO> ChangePasswordAsync(int userId, ChangePasswordDTO password);
+        public Task<UserDetailsDTO> AddShippingAddressAsync(int userId, ShippingAddressDTO shippingAddress);
+        public Task<UserDetailsDTO> RemoveShippingAddressAsync(int userId, ShippingAddressDTO shippingAddress);
+        public Task DeleteAsync(int id);
     }
 }
