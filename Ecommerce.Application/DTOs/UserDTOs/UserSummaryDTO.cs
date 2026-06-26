7ea1c83f8d7887fdfc554ac5794c63eb1aa0da78
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-25 21:13:33 -0300

    feat(user): add UserSummaryDTO

---
+++ Ecommerce.Application/DTOs/UserDTOs/UserSummaryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce.Application.DTOs.UserDTOs
+{
+    public class UserSummaryDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public bool IsAdmin { get; set; }
+    }
+}
