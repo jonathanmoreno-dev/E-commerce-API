9ec612351091e84c5c29134ef93310e367bd3b27
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-25 21:05:37 -0300

    feat(user): add UserUpdateDTO

---
+++ Ecommerce.Application/DTOs/UserDTOs/UserUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Application.DTOs.UserDTOs
+{
+    public class UserUpdateDTO
+    {
+        [MaxLength(150)]
+        public string? FullName { get; set; }
+        [MaxLength(255)]
+        public string? Email { get; set; } 
+        [MaxLength(50)]
+        public string? PhoneNumber { get; set; } 
+        public string? Password { get; set; }
+        public string? AvatarImageUrl { get; set; }
+    }
+}
