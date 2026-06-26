87fef7a3d7e1079f3368e5d0c980ee9330f8646f
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-26 17:22:41 -0300

    feat(user): add GetAllAdminsAsync and GetAllStandardUsersAsync method in IUserRepository

--- Ecommerce.Application/Interfaces/Repositories/IUserRepository.cs
+++ Ecommerce.Application/Interfaces/Repositories/IUserRepository.cs
@@ -5,6 +5,8 @@ namespace Ecommerce.Application.Interfaces.Repositories
     public interface IUserRepository
     {
         public Task<IEnumerable<User>> GetAllAsync();
+        public Task<IEnumerable<User>> GetAllAdminsAsync();
+        public Task<IEnumerable<User>> GetAllStandardUsersAsync();
         public Task<User?> GetByIdAsync(int id);
         public void Add(User user);
         public void Update(User user);
