95985d20797047e3fe803c524f47ebcb5a875c8d
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-26 17:19:37 -0300

    feat(product): add GetAllByCategoryIdAsync method in IProductRepository interface

--- Ecommerce.Application/Interfaces/Repositories/IProductRepository.cs
+++ Ecommerce.Application/Interfaces/Repositories/IProductRepository.cs
@@ -5,6 +5,7 @@ namespace Ecommerce.Application.Interfaces.Repositories
     public interface IProductRepository
     {
         public Task<IEnumerable<Product>> GetAllAsync();
+        public Task<IEnumerable<Product>> GetAllByCategoryIdAsync(int categoryId);
         public Task<Product?> GetByIdAsync(int id);
         public void Add(Product product);
         public void Update(Product product);
