a34870fc16a9a4d6bab63fbb282a1edd6fc95ef3
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-25 21:14:13 -0300

    feat(user): remove Password property to UserUpdateDTO

--- Ecommerce.Application/DTOs/UserDTOs/UserUpdateDTO.cs
+++ Ecommerce.Application/DTOs/UserDTOs/UserUpdateDTO.cs
@@ -10,7 +10,6 @@ namespace Ecommerce.Application.DTOs.UserDTOs
         public string? Email { get; set; } 
         [MaxLength(50)]
         public string? PhoneNumber { get; set; } 
-        public string? Password { get; set; }
         public string? AvatarImageUrl { get; set; }
     }
 }
