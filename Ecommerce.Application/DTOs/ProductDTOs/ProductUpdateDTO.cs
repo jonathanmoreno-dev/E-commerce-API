265f7be320dd8a2195d3d0e78c975dbc4fba29ae
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-25 21:06:14 -0300

    feat(product): add data anottation validations in ProductUpdateDTO

--- Ecommerce.Application/DTOs/ProductDTOs/ProductUpdateDTO.cs
+++ Ecommerce.Application/DTOs/ProductDTOs/ProductUpdateDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace Ecommerce.Application.DTOs.ProductDTOs
 {
     public class ProductUpdateDTO
     {
+        [MaxLength(255)]
         public string? Name { get; set; }
+        [MaxLength(400)]
         public string? ShortDescription { get; set; }
         public string? LongDescription { get; set; }
         public decimal? Price { get; set; }
