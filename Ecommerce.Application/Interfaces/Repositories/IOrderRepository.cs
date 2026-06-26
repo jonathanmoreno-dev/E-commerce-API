01704b899fc6ca78b3ae309586981cfcc36ded1b
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-26 17:12:53 -0300

    feat(order): add GetAllByUserIdAsync and GetAllByStatusAsync method in IOrderService interface

--- Ecommerce.Application/Interfaces/Repositories/IOrderRepository.cs
+++ Ecommerce.Application/Interfaces/Repositories/IOrderRepository.cs
@@ -1,10 +1,13 @@
 ﻿using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.Enums;
 
 namespace Ecommerce.Application.Interfaces.Repositories
 {
     public interface IOrderRepository
     {
         public Task<IEnumerable<Order>> GetAllAsync();
+        public Task<IEnumerable<Order>> GetAllByUserIdAsync(int userId);
+        public Task<IEnumerable<Order>> GetAllByStatusAsync(OrderStatus status);
         public Task<Order?> GetByIdAsync(int id);
         public void Add(Order order);
         public void Update(Order order);
