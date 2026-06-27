4e947bee0a5f0249b11ddb31bc9d48c3a453d445
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-27 18:19:11 -0300

    feat(cart): implement ICartService methods in CartService

--- Ecommerce.Application/Services/CartService.cs
+++ Ecommerce.Application/Services/CartService.cs
@@ -1,42 +1,110 @@
 ﻿using Ecommerce.Application.DTOs.CartDTOs;
 using Ecommerce.Application.DTOs.CartItemDTOs;
+using Ecommerce.Application.Interfaces.Repositories;
 using Ecommerce.Application.Interfaces.Services;
+using Ecommerce.Application.Mappers;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.ValueObjects;
 
 namespace Ecommerce.Application.Services
 {
     public class CartService : ICartService
     {
-        public Task<IEnumerable<CartListDTO>> GetAllAsync()
+        private readonly ICartRepository _cartRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IUserService _userService;
+        private readonly IUnitOfWork _unitOfWork;
+        public CartService(ICartRepository cartRepository, IProductRepository productRepository, IUserService userService, IUnitOfWork unitOfWork)
         {
-            throw new NotImplementedException();
+            _cartRepository = cartRepository;
+            _productRepository = productRepository;
+            _userService = userService;
+            _unitOfWork = unitOfWork;
         }
-        public Task<CartDetailsDTO> GetByIdAsync(int id)
+        public async Task<IEnumerable<CartListDTO>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var carts = await _cartRepository.GetAllAsync();
+
+            var cartListDTOs = carts.Select(x => CartMapper.ToListDTO(x)).ToList();
+            return cartListDTOs;
         }
-        public Task<CartDetailsDTO> GetCurrentUserCartAsync()
+        public async Task<CartDetailsDTO> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var cart = await _cartRepository.GetByIdAsync(id);
+            if (cart is null)
+                throw new KeyNotFoundException($"Cart with Id: {id} was not found");
+
+            var cartDetailsDTO = CartMapper.ToDetailsDTO(cart);
+            return cartDetailsDTO;
         }
-        public Task<CartDetailsDTO> CreateAsync()
+        public async Task<CartDetailsDTO> GetByUserIdAsync(int userId)
         {
-            throw new NotImplementedException();
+            var cart = await _cartRepository.GetByUserIdAsync(userId);
+            if(cart is null)
+                throw new KeyNotFoundException($"Cart with User Id: {userId} was not found");
+
+            var cartDetailsDTO = CartMapper.ToDetailsDTO(cart);
+            return cartDetailsDTO;
         }
-        public Task<CartDetailsDTO> AddItemAsync(CartItemCreateDTO item)
+        public async Task<CartDetailsDTO> GetCurrentUserCartAsync()
         {
-            throw new NotImplementedException();
+            var currentCart = await GetCurrentCartAsync();
+
+            var currentCartDetailsDTO = CartMapper.ToDetailsDTO(currentCart);
+            return currentCartDetailsDTO;
         }
-        public Task<CartDetailsDTO> RemoveItemAsync(int productId)
+        public async Task<CartDetailsDTO> AddItemAsync(CartItemCreateDTO item)
         {
-            throw new NotImplementedException();
+            var currentCart = await GetCurrentCartAsync();
+
+            var product = await _productRepository.GetByIdAsync(item.ProductId);
+            if (product is null)
+                throw new KeyNotFoundException($"Product with Id: {item.ProductId} was not found");
+
+            currentCart.AddItem(product.Id, product.Price, new Quantity(item.Quantity));
+            await _unitOfWork.SaveChangesAsync();
+
+            var currentCartDetailsDTO = CartMapper.ToDetailsDTO(currentCart);
+            return currentCartDetailsDTO;
         }
-        public Task<CartDetailsDTO> UpdateItemAsync(int cartId, CartItemUpdateDTO itemUpdate)
+        public async Task<CartDetailsDTO> RemoveItemAsync(int productId)
         {
-            throw new NotImplementedException();
+            var currentCart = await GetCurrentCartAsync();
+
+            currentCart.RemoveItem(productId);
+            await _unitOfWork.SaveChangesAsync();
+
+            var currentCartDetailsDTO = CartMapper.ToDetailsDTO(currentCart);
+            return currentCartDetailsDTO;
         }
-        public Task<CartDetailsDTO> ClearAsync(int id)
+        public async Task<CartDetailsDTO> UpdateItemAsync(CartItemUpdateDTO itemUpdate)
         {
-            throw new NotImplementedException();
+            var currentCart = await GetCurrentCartAsync();
+
+            currentCart.ChangeItemQuantity(itemUpdate.ProductId, new Quantity(itemUpdate.Quantity));
+            await _unitOfWork.SaveChangesAsync();
+
+            var currentCartDetailsDTO = CartMapper.ToDetailsDTO(currentCart);
+            return currentCartDetailsDTO;
+        }
+        public async Task<CartDetailsDTO> ClearAsync()
+        {
+            var currentCart = await GetCurrentCartAsync();
+
+            currentCart.ClearItems();
+            await _unitOfWork.SaveChangesAsync();
+
+            var currentCartDetailsDTO = CartMapper.ToDetailsDTO(currentCart);
+            return currentCartDetailsDTO;
+        }
+        private async Task<Cart> GetCurrentCartAsync()
+        {
+            var currentUser = await _userService.GetCurrentAsync();
+            var currentCart = await _cartRepository.GetByUserIdAsync(currentUser.Id);
+            if (currentCart is null)
+                throw new KeyNotFoundException($"Cart with User Id: {currentUser.Id} was not found");
+
+            return currentCart;
         }
     }
 }
