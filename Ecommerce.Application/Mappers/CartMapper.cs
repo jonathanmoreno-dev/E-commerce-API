23b145eb47c822dca834459eee736b094521d184
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-27 15:09:13 -0300

    feat(cart): add CartMapper

---
+++ Ecommerce.Application/Mappers/CartMapper.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.Application.DTOs.CartDTOs;
+using Ecommerce.Application.DTOs.CartItemDTOs;
+using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Application.Mappers
+{
+    internal static class CartMapper
+    {
+        public static CartListDTO ToListDTO(Cart cart)
+        {
+            var cartListDTO = new CartListDTO()
+            {
+                Id = cart.Id,
+                UserListDTO = UserMapper.ToListDTO(cart.User)
+            };
+            return cartListDTO;
+        }
+        public static CartDetailsDTO ToDetailsDTO(Cart cart)
+        {
+            var cartDetailsDTO = new CartDetailsDTO()
+            {
+                Id = cart.Id,
+                CreatedAt = cart.CreatedAt,
+                UpdatedAt = cart.UpdatedAt,
+                CartItemListDTOs = cart.CartItems.Select(x => CartItemToListDTO(x)).ToList(),
+                Total = cart.SubTotal.Value
+            };
+            return cartDetailsDTO;
+        }
+        private static CartItemListDTO CartItemToListDTO(CartItem cartItem)
+        {
+            var cartItemListDTO = new CartItemListDTO()
+            {
+                Id = cartItem.Id,
+                ProductSummaryDTO = ProductMapper.ToSummaryDTO(cartItem.Product),
+                UnitPrice = cartItem.UnitPrice.Value,
+                Quantity = cartItem.Quantity.Value,
+                SubTotal = cartItem.Quantity.Value * cartItem.UnitPrice.Value
+            };
+            return cartItemListDTO;
+        }
+    }
+}
