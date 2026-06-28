4ca8c86f1acaf8d06334f5e0800cde9bceaf33ee
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-28 11:43:10 -0300

    feat(category): implement ICategoryService methods to CategoryService

--- Ecommerce.Application/Services/CategoryService.cs
+++ Ecommerce.Application/Services/CategoryService.cs
@@ -1,29 +1,80 @@
 ﻿using Ecommerce.Application.DTOs.CategoryDTOs;
+using Ecommerce.Application.Interfaces.Repositories;
 using Ecommerce.Application.Interfaces.Services;
+using Ecommerce.Application.Mappers;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.ValueObjects;
 
 namespace Ecommerce.Application.Services
 {
     public class CategoryService : ICategoryService
     {
-        public Task<IEnumerable<CategoryListDTO>> GetAllAsync()
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
         {
-            throw new NotImplementedException();
+            _categoryRepository = categoryRepository;
+            _unitOfWork = unitOfWork;
         }
-        public Task<CategoryDetailsDTO> GetByIdAsync(int id)
+        public async Task<IEnumerable<CategoryListDTO>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var categories = await _categoryRepository.GetAllAsync();
+
+            var categoryListDTO = categories.Select(x => CategoryMapper.ToListDTO(x));
+            return categoryListDTO;
         }
-        public Task<CategoryDetailsDTO> CreateAsync(CategoryCreateDTO categoryCreate)
+        public async Task<IEnumerable<CategoryListDTO>> GetAllByProductIdAsync(int productId)
         {
-            throw new NotImplementedException();
+            var categories = await _categoryRepository.GetAllByProductIdAsync(productId);
+
+            var categoryListDTO = categories.Select(x => CategoryMapper.ToListDTO(x));
+            return categoryListDTO;
         }
-        public Task<CategoryDetailsDTO> UpdateAsync(int categoryId, CategoryUpdateDTO categoryUpdate)
+        public async Task<CategoryDetailsDTO> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var category = await _categoryRepository.GetByIdAsync(id);
+            if(category is null)
+                throw new KeyNotFoundException($"Category with Id: {id} was not found");
+
+            var categoryDetailsDTO = CategoryMapper.ToDetailsDTO(category);
+            return categoryDetailsDTO;
         }
-        public Task DeleteAsync(int id)
+        public async Task<CategoryDetailsDTO> CreateAsync(CategoryCreateDTO categoryCreate)
         {
-            throw new NotImplementedException();
+            var category = new Category(new CategoryName(categoryCreate.Name), new CategoryDescription(categoryCreate.Description));
+            _categoryRepository.Add(category);
+            await _unitOfWork.SaveChangesAsync();
+
+            var categoryDetailsDTO = CategoryMapper.ToDetailsDTO(category);
+            return categoryDetailsDTO;
+        }
+        public async Task<CategoryDetailsDTO> UpdateAsync(int categoryId, CategoryUpdateDTO categoryUpdate)
+        {
+            var category = await _categoryRepository.GetByIdAsync(categoryId);
+            if (category is null)
+                throw new KeyNotFoundException($"Category with Id: {categoryId} was not found");
+
+            if(categoryUpdate.Name is not null)
+                category.ChangeName(new CategoryName(categoryUpdate.Name));
+
+            if(categoryUpdate.Description is not null)
+                category.ChangeDescription(new CategoryDescription(categoryUpdate.Description));
+
+            if (categoryUpdate.CategoryImageUrl is not null)
+                category.ChangeCategoryImage(new CategoryImage(categoryUpdate.CategoryImageUrl));
+
+            await _unitOfWork.SaveChangesAsync();
+            var categoryDetailsDTO = CategoryMapper.ToDetailsDTO(category);
+            return categoryDetailsDTO;
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            if (category is null)
+                throw new KeyNotFoundException($"Category with Id: {id} was not found");
+
+            _categoryRepository.Remove(category);
+            await _unitOfWork.SaveChangesAsync();
         }
     }
 }
