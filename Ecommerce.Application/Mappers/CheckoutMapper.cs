c9385f8fde74db8f5788b1b79629d382567f2b44
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-28 13:03:49 -0300

    feat(checkout): add CheckoutMapper

---
+++ Ecommerce.Application/Mappers/CheckoutMapper.cs
@@ -0,0 +1,48 @@
+﻿using Ecommerce.Application.DTOs.CheckoutDTOs;
+using Ecommerce.Application.DTOs.CheckoutItemDTOs;
+using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Application.Mappers
+{
+    internal static class CheckoutMapper
+    {
+        public static CheckoutSummaryDTO ToSummaryDTO(Checkout checkout)
+        {
+            var checkoutSummaryDTO = new CheckoutSummaryDTO()
+            {
+                Id = checkout.Id,
+                CreatedAt = checkout.CreatedAt,
+                ExpiresAt = checkout.ExpiresAt,
+                Total = checkout.Total.Value,
+                PaymentMethod = checkout.PaymentMethod,
+                CheckoutItems = checkout.CheckoutItems.Select(x => CheckoutItemToSummaryDTO(x)).ToList()
+            };
+            return checkoutSummaryDTO;
+        }
+        public static CheckoutDetailsDTO ToDetailsDTO(Checkout checkout)
+        {
+            var checkoutDetailsDTO = new CheckoutDetailsDTO()
+            {
+                Id = checkout.Id,
+                FullAddress = checkout.ShippingAddress.ToString(),
+                SubTotal = checkout.SubTotal.Value,
+                Total = checkout.Total.Value,
+                ShippingCost = checkout.ShippingCost.Value,
+                PaymentMethod = checkout.PaymentMethod,
+                CheckoutItems = checkout.CheckoutItems.Select(x => CheckoutItemToSummaryDTO(x)).ToList()
+            };
+            return checkoutDetailsDTO;
+        }
+        private static CheckoutItemSummaryDTO CheckoutItemToSummaryDTO(CheckoutItem checkoutItem)
+        {
+            var checkoutItemSummaryDTO = new CheckoutItemSummaryDTO()
+            {
+                Id = checkoutItem.Id,
+                Product = ProductMapper.ToSummaryDTO(checkoutItem.Product),
+                Quantity = checkoutItem.Quantity.Value,
+                UnitPrice = checkoutItem.UnitPrice.Value
+            };
+            return checkoutItemSummaryDTO;
+        }
+    }
+}
