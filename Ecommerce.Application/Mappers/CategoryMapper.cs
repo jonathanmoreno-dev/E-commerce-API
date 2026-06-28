dc0aae499fccf8bbcaec4d878800c07ae85ef00c
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-28 11:42:19 -0300

    feat(category): add CategoryMapper

---
+++ Ecommerce.Application/Mappers/CategoryMapper.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.Application.DTOs.CategoryDTOs;
+using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Application.Mappers
+{
+    internal static class CategoryMapper
+    {
+        public static CategoryListDTO ToListDTO(Category category)
+        {
+            var categoryListDTO = new CategoryListDTO()
+            {
+                Id = category.Id,
+                Name = category.Name.Value,
+                CategoryImageUrl = category.CategoryImage?.Url ?? ""
+            };
+            return categoryListDTO;
+        }
+        public static CategoryDetailsDTO ToDetailsDTO(Category category)
+        {
+            var categoryDetailsDTO = new CategoryDetailsDTO()
+            {
+                Id = category.Id,
+                Name = category.Name.Value,
+                Description = category.Description.Value,
+                CategoryImageUrl = category.CategoryImage?.Url ?? "",
+                ProductListDTOs = category.Products.Select(x => ProductMapper.ToListDTO(x)).ToList()
+            };
+            return categoryDetailsDTO;
+        }
+    }
+}
