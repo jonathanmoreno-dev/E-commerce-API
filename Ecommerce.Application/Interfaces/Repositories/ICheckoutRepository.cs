2a189d79afb59afcf4567ad442a0676e6ea7c054
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-06-29 12:49:30 -0300

    feat(checkout): add GetByIdWithPaymentAttemptsAsync method in ICheckoutRepository interface

--- Ecommerce.Application/Interfaces/Repositories/ICheckoutRepository.cs
+++ Ecommerce.Application/Interfaces/Repositories/ICheckoutRepository.cs
@@ -7,6 +7,7 @@ namespace Ecommerce.Application.Interfaces.Repositories
         public Task<IEnumerable<Checkout>> GetAllActiveAsync();
         public Task<IEnumerable<Checkout>> GetAllActiveByUserIdAsync(int userId);
         public Task<Checkout?> GetByIdAsync(int id);
+        public Task<Checkout?> GetByIdWithPaymentAttemptsAsync(int id);
         public void Add(Checkout checkout);
         public void Remove(Checkout checkout);
     }
