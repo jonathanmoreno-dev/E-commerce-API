8b023005892d2c9d4ead5c52e2fa21cee3270524
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-04 14:31:30 -0300

    feat(authentication): add LoginRequestDTO

---
+++ Ecommerce.Application/DTOs/Authentication/LoginRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Application.DTOs.Authentication
+{
+    public class LoginRequestDTO
+    {
+        [MaxLength(255)]
+        [Required]
+        public string Email { get; set; } = "";
+        [Required]
+        public string Password { get; set; } = "";
+    }
+}
