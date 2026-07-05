d923f95a61cb5afc0c9019ab193f574f036d39a6
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-05 16:48:48 -0300

    feat(authentication): implement ICurrentUserService interface properties in CurrentUserService

---
+++ Ecommerce.Infrastructure/Authentication/CurrentUserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using Ecommerce.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+
+namespace Ecommerce.Infrastructure.Authentication
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public Guid UserId { get => Guid.Parse(_httpContextAcessor.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier)!);}
+        public string Name { get => _httpContextAcessor.HttpContext!.User.FindFirstValue(ClaimTypes.Name)!; }
+        public bool IsAdmin { get => _httpContextAcessor.HttpContext!.User.IsInRole("Admin"); }
+
+        private readonly IHttpContextAccessor _httpContextAcessor;
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAcessor = httpContextAccessor;
+        }
+    }
+}
