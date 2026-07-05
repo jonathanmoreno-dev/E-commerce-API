c9f294194152bc414fc6275ab752153df5b84a97
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-05 16:48:06 -0300

    feat(authentication): add ICurrentUserService interface

---
+++ Ecommerce.Application/Interfaces/Services/ICurrentUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Application.Interfaces.Services
+{
+    public interface ICurrentUserService
+    {
+        public Guid UserId { get; }
+        public string Name { get; }
+        public bool IsAdmin { get; }
+    }
+}
