f3662308051142a7f45fbc790ab9776b6c4f0e52
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-06 19:15:33 -0300

    refactor(authentication): reorganize CurrentUserService

--- Ecommerce.Infrastructure/Authentication/CurrentUserService.cs
+++ Ecommerce.Infrastructure/Authentication/CurrentUserService.cs
@@ -6,14 +6,13 @@ namespace Ecommerce.Infrastructure.Authentication
 {
     public class CurrentUserService : ICurrentUserService
     {
-        public Guid UserId { get => Guid.Parse(_httpContextAcessor.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier)!);}
-        public string Name { get => _httpContextAcessor.HttpContext!.User.FindFirstValue(ClaimTypes.Name)!; }
-        public bool IsAdmin { get => _httpContextAcessor.HttpContext!.User.IsInRole("Admin"); }
-
-        private readonly IHttpContextAccessor _httpContextAcessor;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         public CurrentUserService(IHttpContextAccessor httpContextAccessor)
         {
-            _httpContextAcessor = httpContextAccessor;
+            _httpContextAccessor = httpContextAccessor;
         }
+        public Guid UserId { get => Guid.Parse(_httpContextAccessor.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier)!);}
+        public string Name { get => _httpContextAccessor.HttpContext!.User.FindFirstValue(ClaimTypes.Name)!; }
+        public bool IsAdmin { get => _httpContextAccessor.HttpContext!.User.IsInRole("Admin"); }
     }
 }
