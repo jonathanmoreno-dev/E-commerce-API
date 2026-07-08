30016d45927b7cb6f5ee7ab032474eeb0152649e
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-07 21:55:23 -0300

    chore(authentication): add JWT settings

---
+++ Ecommerce.Infrastructure/Authentication/JwtOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Infrastructure.Authentication
+{
+    public class JwtOptions
+    {
+        public const string SectionName = "Jwt";
+        public string Key { get; set; } = "";
+        public string Issuer { get; set; } = "";
+        public string Audience { get; set; } = "";
+        public int AccessTokenExpirationMinutes { get; set; }
+        public int RefreshTokenExpirationDays { get; set; }
+
+    }
+}
