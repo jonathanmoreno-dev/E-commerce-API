a32f49ff344beda93611a3c2ea6e22af9b0c2188
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-07 22:29:43 -0300

    feat(authentication): add ITokenService interface

---
+++ Ecommerce.Application/Interfaces/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Application.Interfaces.Services
+{
+    public interface ITokenService
+    {
+        public string GenerateAccessToken(User user);
+        public string GenerateRefreshToken();
+        public ClaimsPrincipal? GetPrincipalFromExpiredToken(string tokenExpired);
+        public bool ValidateToken(string token);
+    }
+}
