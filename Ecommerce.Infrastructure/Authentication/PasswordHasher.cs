5f6957ce4a40df00b5a0040653698a812f70a133
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-09 13:11:30 -0300

    feat(auth): implement IPasswordHasher interface methods  in PasswordHasher

---
+++ Ecommerce.Infrastructure/Authentication/PasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.Application.Interfaces.Services;
+using BCrypt.Net;
+
+namespace Ecommerce.Infrastructure.Authentication
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool VerifyPassword(string password, string passwordHash) => BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+}
