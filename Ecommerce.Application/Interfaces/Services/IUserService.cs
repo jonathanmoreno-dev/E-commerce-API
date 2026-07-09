71960ead2c20d86b34429c8b30c72f31d9c2d037
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-09 11:22:37 -0300

    refactor(auth): remove RegisterAsync and LoginAsync method from IUserService interface and UserService

--- Ecommerce.Application/Interfaces/Services/IUserService.cs
+++ Ecommerce.Application/Interfaces/Services/IUserService.cs
@@ -11,10 +11,8 @@ namespace Ecommerce.Application.Interfaces.Services
         public Task<IEnumerable<UserSummaryDTO>> GetAllStandardUsersAsync();
         public Task<UserDetailsDTO> GetByIdAsync(Guid id);
         public Task<UserDetailsDTO> GetCurrentAsync();
-        public Task<UserDetailsDTO> RegisterAsync(RegisterRequestDTO userRegister);
-        public Task<UserDetailsDTO> LoginAsync(LoginRequestDTO userLogin);
         public Task<UserDetailsDTO> UpdateAsync(Guid userId, UserUpdateDTO userUpdate);
-        public Task<UserDetailsDTO> ChangePasswordAsync(Guid userId, ChangePasswordDTO password);
+        public Task ChangePasswordAsync(Guid userId, ChangePasswordDTO password);
         public Task<UserDetailsDTO> AddShippingAddressAsync(Guid userId, ShippingAddressDTO shippingAddress);
         public Task<UserDetailsDTO> RemoveShippingAddressAsync(Guid userId, ShippingAddressDTO shippingAddress);
         public Task DeleteAsync(Guid id);
