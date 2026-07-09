d41a82de046a78ac0e43147b3140f0c6ce5802c4
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-09 16:19:13 -0300

    feat(auth): add RefreshTokenConfiguration class to configure entity mappings

---
+++ Ecommerce.Infrastructure/Configuration/RefreshTokenConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Configuration
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.ToTable("refresh_tokens");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName("id");
+            builder.Property(x => x.Token).IsRequired().HasColumnName("token").HasMaxLength(500);
+            builder.Property(x => x.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP").HasColumnName("created_at");
+            builder.Property(x => x.ExpiresAt).HasColumnName("expires_at");
+            builder.Property(x => x.RevokedAt).HasColumnName("revoked_at");
+            builder.Property(x => x.UserId).HasColumnName("user_id");
+            builder.HasIndex(x => x.Token).IsUnique();
+            builder.HasOne(x => x.User).WithMany(y => y.RefreshTokens).HasForeignKey(x => x.UserId);
+        }
+    }
+}
