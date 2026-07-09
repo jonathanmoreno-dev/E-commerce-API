4593d6b2f559ee2db19d506af8cf86d9d44a1b15
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-09 13:12:01 -0300

    feat(auth): add AuthResponseDTO

---
+++ Ecommerce.Application/DTOs/Authentication/AuthResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Application.DTOs.Authentication
+{
+    public class AuthResponseDTO
+    {
+        public string AcessToken { get; set; } = "";
+        public string RefreshToken { get; set; } = "";
+    }
+}
