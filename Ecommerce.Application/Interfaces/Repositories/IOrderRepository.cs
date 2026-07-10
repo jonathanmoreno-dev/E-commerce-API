e161fb841acbb1138b3eff9a7f8229b56505ea79
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-10 13:51:51 -0300

    refactor(order): separate detailed order queries

--- Ecommerce.Application/Interfaces/Repositories/IOrderRepository.cs
+++ Ecommerce.Application/Interfaces/Repositories/IOrderRepository.cs
@@ -9,6 +9,7 @@ namespace Ecommerce.Application.Interfaces.Repositories
         public Task<IEnumerable<Order>> GetAllByUserIdAsync(Guid userId);
         public Task<IEnumerable<Order>> GetAllByUserIdAndStatusAsync(Guid userId, OrderStatus status);
         public Task<Order?> GetByIdAsync(Guid id);
+        public Task<Order?> GetByIdForDetailsAsync(Guid id);
         public void Add(Order order);
     }
 }
