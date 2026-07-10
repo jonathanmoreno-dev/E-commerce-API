e4cb24919ef05787285fb6a0daa50028f0da7001
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-10 11:05:19 -0300

    feat(auth): add IRefreshTokenRepository interface

---
+++ Ecommerce.Application/Interfaces/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Application.Interfaces.Repositories
+{
+    public interface IRefreshTokenRepository
+    {
+        public Task<IEnumerable<RefreshToken>> GetAllAsync();
+        public Task<IEnumerable<RefreshToken>> GetAllByUserIdAsync(Guid userId);
+        public Task<RefreshToken?> GetActiveByUserIdAsync(Guid userId);
+        public Task<RefreshToken?> GetByIdAsync(Guid id);
+        public Task<RefreshToken?> GetByTokenAsync(string token);
+        public void Add(RefreshToken refreshToken);
+        public void Remove(RefreshToken refreshToken);
+    }
+}
