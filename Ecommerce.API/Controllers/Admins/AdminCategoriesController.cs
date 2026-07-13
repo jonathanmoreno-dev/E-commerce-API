08ff369fedfd57a713e9c6d802e2d5698fb1198b
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-13 14:27:24 -0300

    feat(category): implement category controllers

---
+++ Ecommerce.API/Controllers/Admins/AdminCategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.Application.DTOs.CategoryDTOs;
+using Ecommerce.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers.Admins
+{
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    [Route("api/admin/categories")]
+    public class AdminCategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public AdminCategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<CategoryDetailsDTO>> GetById(Guid id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return Ok(category);
+        }
+        [HttpPost]
+        public async Task<ActionResult<CategoryDetailsDTO>> Create(CategoryCreateDTO requestDTO)
+        {
+            var category = await _categoryService.CreateAsync(requestDTO);
+            return CreatedAtAction(nameof(GetById), new { id = category.Id }, category);
+        }
+        [HttpPatch("{id:guid}")]
+        public async Task<ActionResult<CategoryDetailsDTO>> Update(Guid id, CategoryUpdateDTO requestDTO)
+        {
+            var category = await _categoryService.UpdateAsync(id, requestDTO);
+            return Ok(category);
+        }
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await _categoryService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
