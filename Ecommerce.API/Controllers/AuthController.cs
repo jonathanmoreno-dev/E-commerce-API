96cbb5b9699933b791e3d9b16fa83f68ea5ac46b
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-13 13:23:35 -0300

    feat(auth): implement AuthController (#12)

---
+++ Ecommerce.API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce.Application.DTOs.Authentication;
+using Ecommerce.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [ApiController]
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthResponseDTO>> Register(RegisterRequestDTO requestDTO)
+        {
+            var authResponseDTO = await _authService.RegisterAsync(requestDTO);
+            return Ok(authResponseDTO);
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponseDTO>> Login(LoginRequestDTO requestDTO)
+        {
+            var authResponseDTO = await _authService.LoginAsync(requestDTO);
+            return Ok(authResponseDTO);
+        }
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<AuthResponseDTO>> RefreshToken(RefreshTokenRequestDTO requestDTO)
+        {
+            var authResponseDTO = await _authService.RefreshTokenAsync(requestDTO.RefreshToken);
+            return Ok(authResponseDTO);
+        }
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout(RefreshTokenRequestDTO requestDTO)
+        {
+            await _authService.LogoutAsync(requestDTO.RefreshToken);
+            return NoContent();
+        }
+    }
+}
