d19a0cf16984481e31da2c43ed5b19a97087f4b8
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-13 14:33:24 -0300

    feat(checkout): implement checkout controllers

---
+++ Ecommerce.API/Controllers/CheckoutsController.cs
@@ -0,0 +1,53 @@
+﻿using Ecommerce.Application.DTOs.CheckoutDTOs;
+using Ecommerce.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CheckoutsController : ControllerBase
+    {
+        private readonly ICheckoutService _checkoutService;
+        public CheckoutsController(ICheckoutService checkoutService)
+        {
+            _checkoutService = checkoutService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CheckoutSummaryDTO>>> GetAllCurrentUserCheckouts()
+        {
+            var checkouts = await _checkoutService.GetAllCurrentUserCheckoutsActiveAsync();
+            return Ok(checkouts);
+        }
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<CheckoutDetailsDTO>> GetById(Guid id)
+        {
+            var checkout = await _checkoutService.GetByIdAsync(id);
+            return Ok(checkout);
+        }
+        [HttpPost]
+        public async Task<ActionResult<CheckoutDetailsDTO>> Create(CheckoutCreateDTO requestDTO)
+        {
+            var checkout = await _checkoutService.CreateAsync(requestDTO);
+            return CreatedAtAction(nameof(GetById), new { id = checkout.Id }, checkout);
+        }
+        [HttpPatch("{id:guid}")]
+        public async Task<ActionResult<CheckoutDetailsDTO>> Update(Guid id, CheckoutUpdateDTO requestDTO)
+        {
+            var checkout = await _checkoutService.UpdateAsync(id, requestDTO);
+            return Ok(checkout);
+        }
+        [HttpPost("{id:guid}/payment")]
+        public async Task<IActionResult> CreatePayment(Guid id)
+        {
+            await _checkoutService.CreatePaymentAsync(id);
+            return NoContent();
+        }
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _checkoutService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
