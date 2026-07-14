08eaa61b864cc152be1300410f4cb0a00b99d32f
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-14 18:01:18 -0300

    feat(auth): add RefreshTokenRequestDTO (#21)
    ## Changes
    
    Add RefreshTokenRequestDTO

---
+++ Ecommerce.Application/DTOs/Authentication/RefreshTokenRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Application.DTOs.Authentication
+{
+    public class RefreshTokenRequestDTO
+    {
+        [Required]
+        public string RefreshToken { get; set; } = "";
+    }
+}
