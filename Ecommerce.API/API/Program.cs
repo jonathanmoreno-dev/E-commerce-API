a68ad36a5b5c9f12f340d60d14cf894c083ee20f
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-18 16:35:12 -0300

    chore(api): configure JSON enum string serialization (#27)
    ## Summary
    
    Configure the API to serialize and deserialize enums as strings using
    `JsonStringEnumConverter`.
    
    ## Changes
    
    - Added `JsonStringEnumConverter` to the JSON serialization options.
    - Enabled string-based enum values for request and response bodies.

--- Ecommerce.API/API/Program.cs
+++ Ecommerce.API/API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json.Serialization;
 using Ecommerce.Application.Interfaces.Repositories;
 using Ecommerce.Application.Interfaces.Services;
 using Ecommerce.Application.Services;
@@ -15,7 +16,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Configuration.AddUserSecrets<Program>();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
