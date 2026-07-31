fa3e28c55f7f6d320cb874576302dcd2d757d174
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-07-31 12:49:57 -0300

    feat(url): add Url validations to AvatarImage, CategoryImage and ProductImage value objects

--- Ecommerce.Domain/ValueObjects/AvatarImage.cs
+++ Ecommerce.Domain/ValueObjects/AvatarImage.cs
@@ -10,8 +10,15 @@ namespace Ecommerce.Domain.ValueObjects
         {
             if (string.IsNullOrWhiteSpace(url))
                 throw new DomainValidationException("AvatarImageUrl cannot be empty");
+            url = url.Trim();
+            if (url.Length > 2048)
+                throw new DomainValidationException("URL cannot exceed 2048 characters");
+            if (!Uri.TryCreate(url, UriKind.Absolute, out var uri))
+                throw new DomainValidationException("Invalid Url");
+            if (uri.Scheme != Uri.UriSchemeHttps && uri.Scheme != Uri.UriSchemeHttp)
+                throw new DomainValidationException("Only HTTP and HTTPS URLs are allowed");
 
-            Url = url;   
+            Url = uri.AbsoluteUri;   
         }
     }
 }
--- Ecommerce.Domain/ValueObjects/ProductImage.cs
+++ Ecommerce.Domain/ValueObjects/ProductImage.cs
@@ -13,8 +13,15 @@ namespace Ecommerce.Domain.ValueObjects
                 throw new DomainValidationException("ProductImageUrl cannot be empty");
             if (order <= 0)
                 throw new DomainValidationException("Order must be greater than 0");
+            url = url.Trim();
+            if(url.Length > 2048)
+                throw new DomainValidationException("URL cannot exceed 2048 characters");
+            if(!Uri.TryCreate(url, UriKind.Absolute, out var uri))
+                throw new DomainValidationException("Invalid Url");
+            if(uri.Scheme != Uri.UriSchemeHttps && uri.Scheme != Uri.UriSchemeHttp)
+                throw new DomainValidationException("Only HTTP and HTTPS URLs are allowed");
 
-            Url = url;
+            Url = uri.AbsoluteUri;
             Order = order;
         }
     }
