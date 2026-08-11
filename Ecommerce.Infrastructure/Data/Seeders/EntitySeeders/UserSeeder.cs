8fbe6d9dd5c3fd2c0a7e858e95244a87c762d43a
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-08-11 16:13:18 -0300

    feat(seeder): add development user seeder (#60)
    ## Summary
    
    Generate users with hashed passwords and avatar images for development.
    
    ## Changes
    
    * Added a dedicated user seeder.
    * Generated exactly 100 users through `UserFaker`.
    * Extracted each email prefix to construct its development password.
    * Generated passwords using the `{emailPrefix}Password` format.
    * Hashed every password through `IPasswordHasher`.
    * Replaced the temporary faker password hash.
    * Added a generated avatar image to every user.
    * Added the users to the database context for later persistence.

---
+++ Ecommerce.Infrastructure/Data/Seeders/EntitySeeders/UserSeeder.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce.Application.Interfaces.Services;
+using Ecommerce.Domain.ValueObjects;
+using Ecommerce.Fakers;
+
+namespace Ecommerce.Infrastructure.Data.Seeders.EntitySeeders
+{
+    public class UserSeeder
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IPasswordHasher _passwordHasher;
+        public UserSeeder(AppDbContext appDbContext, IPasswordHasher passwordHasher)
+        {
+            _appDbContext = appDbContext;
+            _passwordHasher = passwordHasher;
+        }
+        public Task SeedAsync()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var user = UserFaker.Create();
+                var emailPrefix = user.Email.Value.Split('@')[0];
+                var passwordHash = _passwordHasher.HashPassword($"{emailPrefix}Password");
+                user.ChangePasswordHash(passwordHash);
+                user.ChangeAvatarImage(new AvatarImage(UserFaker.GetAvatarImageUrl()));
+                _appDbContext.Users.Add(user);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
