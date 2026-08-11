14e5d46600fd9c92ebd56e1dc6f61498ddf4f7db
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-08-11 19:51:36 -0300

    feat(seeder): add development cart seeder

---
+++ Ecommerce.Infrastructure/Data/Seeders/EntitySeeders/CartSeeder.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.ValueObjects;
+using Ecommerce.Fakers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Data.Seeders.EntitySeeders
+{
+    public class CartSeeder
+    {
+        private readonly AppDbContext _appDbContext;
+        public CartSeeder(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task SeedAsync()
+        {
+            var products = await _appDbContext.Products.ToListAsync();
+            var users = await _appDbContext.Users.Where(x => x.Cart == null).ToListAsync();
+            foreach (var user in users)
+            {
+                var cart = CartFaker.CreateRealCart(user.Id, products);
+                _appDbContext.Carts.Add(cart);
+            }
+        }
+    }
+}
