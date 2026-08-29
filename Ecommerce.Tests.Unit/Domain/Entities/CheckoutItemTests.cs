d42462f6409a908358fe67d93c63bbb9f2316394
Jonathan Moreno <jonathanmoreno.dev@gmail.com>
2026-08-29 18:49:23 -0300

    test(checkout-item): add entity unit tests

---
+++ Ecommerce.Tests.Unit/Domain/Entities/CheckoutItemTests.cs
@@ -0,0 +1,54 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.ValueObjects;
+
+namespace Ecommerce.Tests.Unit.Domain.Entities
+{
+    public class CheckoutItemTests
+    {
+        [Fact]
+        public void ShouldCreateCheckoutItemWithValidValues()
+        {
+            var productId = Guid.NewGuid();
+            var unitPrice = new Money(99.90m);
+            var quantity = new Quantity(2);
+
+            var checkoutItem = new CheckoutItem(productId, unitPrice, quantity);
+
+            Assert.NotEqual(Guid.Empty, checkoutItem.Id);
+            Assert.Equal(productId, checkoutItem.ProductId);
+            Assert.Equal(unitPrice, checkoutItem.UnitPrice);
+            Assert.Equal(quantity, checkoutItem.Quantity);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWhenUnitPriceIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CheckoutItem(Guid.NewGuid(), null!, new Quantity(2)));
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWhenQuantityIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CheckoutItem(Guid.NewGuid(), new Money(99.90m), null!));
+        }
+
+        [Fact]
+        public void ShouldIncreaseQuantity()
+        {
+            var checkoutItem = new CheckoutItem(Guid.NewGuid(), new Money(99.90m), new Quantity(2));
+            var quantityToAdd = new Quantity(3);
+
+            checkoutItem.IncreaseQuantity(quantityToAdd);
+
+            Assert.Equal(new Quantity(5), checkoutItem.Quantity);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWhenIncreasingQuantityWithNull()
+        {
+            var checkoutItem = new CheckoutItem(Guid.NewGuid(), new Money(99.90m), new Quantity(2));
+
+            Assert.Throws<ArgumentNullException>(() => checkoutItem.IncreaseQuantity(null!));
+        }
+    }
+}
